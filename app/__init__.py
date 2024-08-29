cf0aae76b11f48034a14a2e85f975d35a01c38cf
huy.tang99 <huy.tang99@hcmut.edu.vn>
2024-08-29 23:31:44 +0700

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy 
+from azure.servicebus import QueueClient
+
+
+app = Flask(__name__)
+app.config.from_object('config.DevelopmentConfig')
+
+app.secret_key = app.config.get('SECRET_KEY')
+
+conn_str = app.config.get('SERVICE_BUS_CONNECTION_STRING')
+servicebus_qname = app.config.get('SERVICE_BUS_QUEUE_NAME')
+
+db = SQLAlchemy(app)
+
+from . import routes
